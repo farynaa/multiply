51b6d7dda448643b2ea7f1e122228bf568f82b97
farynaa <farynaa@yandex.ru>
2019-10-01 12:09:49 +0300

    init: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-module.exports = function multiply(first, second) {
-  // your solution
+const multiply = (a, b) => {
+
+    let resultArray = Array(a.length + b.length + 1).fill(0);
+
+    const A = a.split('').reverse();
+    const B = b.split('').reverse();
+
+    for (let i = 0; i < A.length; i ++ ) {
+        for (let j = 0; j < B.length; j ++) {
+            const divisionResult = A[i] * B[j];
+            resultArray[i + j] += divisionResult;
+        }
+    }
+
+    for (let i = 0; i < resultArray.length; i ++) {
+        if (resultArray[i] > 9) {
+            resultArray[i + 1] += Math.floor(resultArray[i] / 10);
+            resultArray[i] = resultArray[i] % 10
+        }
+    }
+    
+    return resultArray.reverse().join('').replace(/^0+/, '');
 }
+
+module.exports = multiply;
+//const result = multiply('329568934658432659586', '58379426534596');
+
+//console.log(result);
+//console.log( 25 * 136)
+
